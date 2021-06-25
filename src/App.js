87748bb947f1a3f21d38ca993c995865dc25766c
robijntje <44802556+robijntje@users.noreply.github.com>
2021-06-25 11:29:20 +0200

    Added distinction between sent and received messages
    Added system to identify sent or reveived messages based in user id (uid).
    Added photoURL for users to have a distinctive photo.

--- src/App.js
+++ src/App.js
@@ -69,8 +69,15 @@ return (
 }
 
 function ChatMessage(props) {
-  const { text, uid } = props.message;
-  return <p>{text}</p>
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  return (
+  <div className={'message ${messageClass}'}>
+    <img src={photoURL} />
+    <p>{text}</p>
+    </div>
+    )
 }
 
 export default App;
